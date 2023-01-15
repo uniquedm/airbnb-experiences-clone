e1783d07e5d3238b36fb8f80392df1260fb0f5fe
Devansh Mishra <devanshunique@gmail.com>
2023-01-15 23:31:06 +0530

    Hero Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
 
 export default function App() {
     return (
         <div>
             <NavBar />
+            <Hero />
         </div>
     )
 }

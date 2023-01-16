0a46361c546f6a8747c25f246c1ac64944d5844a
Devansh Mishra <devanshunique@gmail.com>
2023-01-16 23:39:02 +0530

    Static Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
+import Card from "./components/Card"
 
 export default function App() {
     return (
         <div>
             <NavBar />
             <Hero />
+            <Card />
         </div>
     )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import image from "../wedding-photo.png"
+import star from "../star.png"
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img className="card-image" src={image} alt = "content" />
+            <div className="rating-div">
+                <img className="card-star" src={star} alt = "content" />
+                <p><b>5.0</b></p>
+                <p>(30)</p>
+                <p>.</p>
+                <p>USA</p>
+            </div>
+            <p>Learn wedding photography</p>
+            <p><b>From $125</b> / person</p>
+        </div>
+    )
+}

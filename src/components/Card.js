d31cee978aecdbb79795f7a90bbcf25f77ad8881
Devansh Mishra <devanshunique@gmail.com>
2023-01-20 23:44:35 +0530

    Object Destructuring

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,25 @@
 import React from "react";
 import star from "../images/star.png"
 
-export default function Card(props) {
-    
+export default function Card({img, status, title, rating, ratingCount, price}) {
     let imageStyle = "card-image"
-    if (props.status==="Sold Out") {
+    if (status==="Sold Out") {
         imageStyle = "card-image-gray"
     }
 
     return (
         <div className="card">
-            <img className={imageStyle} src={props.img} alt = "content" />
+            <img className={imageStyle} src={img} alt = "content" />
             <div className="rating-div">
                 <img className="card-star" src={star} alt = "content" />
-                <p><b>{props.rating}</b></p>
-                <p>({props.ratingCount})</p>
+                <p><b>{rating}</b></p>
+                <p>({ratingCount})</p>
                 <p>.</p>
                 <p>USA</p>
             </div>
-            <p>{props.title}</p>
-            <p><b>From {props.price}</b> / person</p>
-            <p className="card-status">{props.status}</p>
+            <p>{title}</p>
+            <p><b>From {price}</b> / person</p>
+            <p className="card-status">{status}</p>
         </div>
     )
 }

f107b49ed6e2aeb95256dde4b06c3ef3064c6107
Devansh Mishra <devanshunique@gmail.com>
2023-01-20 23:32:35 +0530

    Graying Out Sold Out Experiences

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,15 @@ import React from "react";
 import star from "../images/star.png"
 
 export default function Card(props) {
+    
+    let imageStyle = "card-image"
+    if (props.status==="Sold Out") {
+        imageStyle = "card-image-gray"
+    }
+
     return (
         <div className="card">
-            <img className="card-image" src={props.img} alt = "content" />
+            <img className={imageStyle} src={props.img} alt = "content" />
             <div className="rating-div">
                 <img className="card-star" src={star} alt = "content" />
                 <p><b>{props.rating}</b></p>
